246524e70bb078c21df1bb14339d673dfc6ef5d4
Min-h-96 <asdf6256@gmail.com>
2020-09-24 23:45:10 +0900

    #6 make load more button

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -5,20 +5,33 @@ import MainImage from "./Section/MainImage";
 import GridCards from "../../commons/GridCards";
 import { Row } from "antd";
 
-function LandingPage(props) {
+function LandingPage() {
   const [Movies, setMovies] = useState([]);
   const [MainMovieImage, setMainMovieImage] = useState(null);
+  const [CurrentPage, setCurrentPage] = useState(0);
 
   useEffect(() => {
     const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
 
+    fetchMovies(endpoint);
+  }, []);
+
+  const fetchMovies = (endpoint) => {
     fetch(endpoint)
       .then((response) => response.json())
       .then((response) => {
-        setMovies([...response.results]);
-        setMainMovieImage(response.results[0]);
+        console.log(response);
+        setMovies([...Movies, ...response.results]);
+        setMainMovieImage(...Movies, response.results[0]);
+        setCurrentPage(response.page);
       });
-  }, []);
+  };
+
+  const loadMoreItems = () => {
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${CurrentPage + 1}`;
+
+    fetchMovies(endpoint);
+  };
 
   return (
     <div style={{ width: "100%", margin: "0" }}>
@@ -48,7 +61,9 @@ function LandingPage(props) {
         </Row>
       </div>
       <div style={{ display: "flex", justifyContent: "center" }}>
-        <button>Load More</button>
+        <button onClick={loadMoreItems} style={{ marginBottom: "1rem" }}>
+          Load More
+        </button>
       </div>
     </div>
   );
