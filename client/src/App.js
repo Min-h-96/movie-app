431094184a0c912e4062be73d1ff21155016807a
Min-h-96 <asdf6256@gmail.com>
2020-09-27 14:27:50 +0900

    Add Navbar and Footer

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,16 @@ import LandingPage from "./components/views/LandingPage/LandingPage";
 import LoginPage from "./components/views/LoginPage/LoginPage";
 import RegisterPage from "./components/views/RegisterPage/RegisterPage";
 import MovieDetail from "./components/views/MovieDetail/MovieDetail";
+import FavoritePage from "./components/views/FavoritePage/FavoritePage";
+import Navbar from "./components/views/NavBar/NavBar";
+import Footer from "./components/views/Footer/Footer";
+
 import Auth from "./hoc/auth";
+import Favorite from "./components/views/MovieDetail/Section/Favorite";
 function App() {
   return (
     <Router>
+      <Navbar />
       <div>
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
@@ -18,8 +24,10 @@ function App() {
           <Route exact path="/login" component={Auth(LoginPage, false)} />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
           <Route exact path="/movie/:movieId" component={Auth(MovieDetail, null)}></Route>
+          <Route exact path="/favorite" component={Auth(Favorite, true)} />
         </Switch>
       </div>
+      <Footer />
     </Router>
   );
 }
